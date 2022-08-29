ec655fe3427f757b63cb3163872cd1ac6754019a
Nemanja <ladro_magro@yahoo.com>
2022-08-29 11:07:05 -0400

    first initial

--- src/main/java/com/solvd/hospitalpatientrecoverymodel/string/UtilsMain.java
+++ src/main/java/com/solvd/hospitalpatientrecoverymodel/string/UtilsMain.java
@@ -7,5 +7,6 @@ import static com.solvd.hospitalpatientrecoverymodel.string.StringTest.metoda;
 public class UtilsMain {
     public static void main(String[] args) throws Exception {
         metoda();
+        System.out.println("Whatever");
     }
 }
